05d873cf162446c045296d507dbf0aa5888c8f46
vadimdidenkogs <vadim.didenko@482.solutions>
2019-07-12 11:30:59 +0300

    SP-502 | Implement GetAccountNames in GO SDK * Update year of copyright. * Remove init method

--- test/integration/account_test.go
+++ test/integration/account_test.go
@@ -1,37 +1,15 @@
-// Copyright 2018 ProximaX Limited. All rights reserved.
+// Copyright 2019 ProximaX Limited. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 package integration
 
 import (
-	"context"
 	"github.com/proximax-storage/go-xpx-catapult-sdk/sdk"
-	"github.com/proximax-storage/go-xpx-catapult-sdk/sdk/websocket"
 	"github.com/proximax-storage/go-xpx-utils/tests"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func init() {
-	cfg, err := sdk.NewConfigFromRemote([]string{testUrl})
-	if err != nil {
-		panic(err)
-	}
-
-	ctx = context.Background()
-	client = sdk.NewClient(nil, cfg)
-
-	wsc, err = websocket.NewClient(ctx, cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	defaultAccount, err = client.NewAccountFromPrivateKey(privateKey)
-	if err != nil {
-		panic(err)
-	}
-}
 
 func TestAddressService_GetAccountNames(t *testing.T) {
 
