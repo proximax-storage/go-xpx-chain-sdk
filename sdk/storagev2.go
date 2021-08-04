d63d4c4b54ec25d4179f29031e9ab31b34f0c93b
wernling-proximax <wernling.tan@proximax.io>
2021-08-04 19:32:54 +0800

    Adding DTO for storagev2

--- sdk/storagev2.go
+++ sdk/storagev2.go
@@ -4,4 +4,33 @@
 
 package sdk
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/proximax-storage/go-xpx-utils/net"
+)
+
 type StorageV2Service service
+
+func (s *StorageV2Service) GetDrive(ctx context.Context, driveKey *PublicAccount) (*BcDrive, error) {
+	if driveKey == nil {
+		return nil, ErrNilAddress
+	}
+
+	url := net.NewUrl(fmt.Sprintf(driveRoute, driveKey.PublicKey))
+
+	dto := &bcDriveDTO{}
+
+	resp, err := s.client.doNewRequest(ctx, http.MethodGet, url.Encode(), nil, dto)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = handleResponseStatusCode(resp, map[int]error{404: ErrResourceNotFound, 409: ErrArgumentNotValid}); err != nil {
+		return nil, err
+	}
+
+	return dto.toStruct(s.client.NetworkType())
+}
