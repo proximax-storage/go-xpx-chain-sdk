7797b98148942904e9b94a4cad2e57099490ec81
jackshenn <chanjackshen@gmail.com>
2021-10-04 14:35:21 +0800

    storagev2 schema and flatbuffer

---
+++ sdk/storagev2_schema.go
@@ -0,0 +1,19 @@
+// Copyright 2021 ProximaX Limited. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package sdk
+
+func replicatorOffboardingTransactionSchema() *schema {
+	return &schema{
+		[]schemaAttribute{
+			newScalarAttribute("size", IntSize),
+			newArrayAttribute("signature", ByteSize),
+			newArrayAttribute("signer", ByteSize),
+			newScalarAttribute("version", IntSize),
+			newScalarAttribute("type", ShortSize),
+			newArrayAttribute("maxFee", IntSize),
+			newArrayAttribute("deadline", IntSize),
+		},
+	}
+}
