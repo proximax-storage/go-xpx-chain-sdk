4b24a3bd29f720b081ffb3a1f0f9482a1abb0882
WernLing <wernling.tan@proximax.io>
2022-05-09 14:59:53 +0800

    Add exchangesda schemas and flatbuffers

---
+++ sdk/exchangesda_schema.go
@@ -0,0 +1,51 @@
+// Copyright 2022 ProximaX Limited. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package sdk
+
+func placeSdaExchangeOfferTransactionSchema() *schema {
+	return &schema{
+		[]schemaAttribute{
+			newScalarAttribute("size", IntSize),
+			newArrayAttribute("signature", ByteSize),
+			newArrayAttribute("signer", ByteSize),
+			newScalarAttribute("version", IntSize),
+			newScalarAttribute("type", ShortSize),
+			newArrayAttribute("maxFee", IntSize),
+			newArrayAttribute("deadline", IntSize),
+			newScalarAttribute("sdaOfferCount", ByteSize),
+			newTableArrayAttribute("sdaOffers", schema{
+				[]schemaAttribute{
+					newArrayAttribute("mosaicIdGive", IntSize),
+					newArrayAttribute("mosaicAmountGive", IntSize),
+					newArrayAttribute("mosaicIdGet", IntSize),
+					newArrayAttribute("mosaicAmountGet", IntSize),
+					newArrayAttribute("owner", ByteSize),
+					newArrayAttribute("duration", IntSize),
+				},
+			}.schemaDefinition),
+		},
+	}
+}
+
+func removeSdaExchangeOfferTransactionSchema() *schema {
+	return &schema{
+		[]schemaAttribute{
+			newScalarAttribute("size", IntSize),
+			newArrayAttribute("signature", ByteSize),
+			newArrayAttribute("signer", ByteSize),
+			newScalarAttribute("version", IntSize),
+			newScalarAttribute("type", ShortSize),
+			newArrayAttribute("maxFee", IntSize),
+			newArrayAttribute("deadline", IntSize),
+			newScalarAttribute("sdaOfferCount", ByteSize),
+			newTableArrayAttribute("sdaOffers", schema{
+				[]schemaAttribute{
+					newArrayAttribute("mosaicIdGive", IntSize),
+					newArrayAttribute("mosaicIdGet", IntSize),
+				},
+			}.schemaDefinition),
+		},
+	}
+}
