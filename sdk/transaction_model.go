0cb71c3ac14b8167167ee0911e3ce1a5e0df6bd4
jackshenn <chanjackshen@gmail.com>
2024-08-21 17:51:23 +0800

    add bson transfer

--- sdk/transaction_model.go
+++ sdk/transaction_model.go
@@ -1806,9 +1806,9 @@ func (tx *MosaicRemoveLevyTransaction) Size() int {
 
 type TransferTransaction struct {
 	AbstractTransaction
-	Message   Message
-	Mosaics   []*Mosaic
-	Recipient *Address
+	Message   Message `bson:"message"`
+	Mosaics   []*Mosaic `bson:"mosaics"`
+	Recipient *Address `bson:"address"`
 }
 
 // returns a TransferTransaction from passed transfer recipient Adderess, array of Mosaic's to transfer and transfer Message
